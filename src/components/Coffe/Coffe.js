ad7ed466a3d8c998d6f8ba3682de6ff0fc11f5d7
= <milosgra@gmail.com>
2019-08-21 12:16:31 +0200

    second commit on master branch

--- src/components/Coffe/Coffe.js
+++ src/components/Coffe/Coffe.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import changeImage from `${process.env.PUBLIC_URL}/img/change.jpg`;
-import './Coffe.css';
+import "./Coffe.css";
 
 class Coffe extends Component {
-
   render() {
     return (
       <div>
-        {
-          this.props.coffeImage ?
-            <div className="card border border-dark change-bg my-3">
-                <h5 className="card-header change-bg">Your Coffee</h5>
-                <div className="card-body">
-                  <img src={`/img/${this.props.coffe.img}`} alt="test" className="card-img-top coffe-image"/>
-                </div>
+        {this.props.coffeImage ? (
+          <div className="card border border-dark change-bg my-3">
+            <h5 className="card-header change-bg">Your Coffee</h5>
+            <div className="card-body">
+              <img
+                src={`/img/${this.props.coffe.img}`}
+                alt="test"
+                className="card-img-top coffe-image"
+              />
             </div>
-          : null
-        }
+          </div>
+        ) : null}
       </div>
     );
   }
-
 }
-
 
 export default Coffe;
